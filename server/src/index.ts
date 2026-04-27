3da30cd35ff4465cc1eb99142da32ac34050a6b1
Lahatranomena <hei.lahatra.3@gmail.com>
2026-04-27 15:15:53 +0300

    refactor(room): rename file route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import "dotenv/config";
 import authRoutes from './routes/auth.route'
-import roomRoutes from './routes/room.route'
+import roomRoutes from './routes/rooms.route'
 const app = express()
 
 const PORT = Number(process.env.PORT) || 3001
