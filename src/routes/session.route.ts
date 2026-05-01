b9b86ab0b2a15c1045669039d5aebef33761fa63
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-05-01 13:23:45 +0300

    Protect route session

--- src/routes/session.route.ts
+++ src/routes/session.route.ts
@@ -1,10 +1,11 @@
 import { SessionController } from "../controllers/session.controller";
 import { Router } from "express";
+import { authMiddleware } from "../middlewares/admin.middleware";
 
 const router = Router();
 const sessionController = new SessionController();
 
-router.post("/", sessionController.postSession);
-router.put("/:id", sessionController.putSession);
+router.post("/", authMiddleware, sessionController.postSession);
+router.put("/:id",authMiddleware, sessionController.putSession);
 
 export default router;
